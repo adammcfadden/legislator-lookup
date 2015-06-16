defdf53d843ccdc0a44677d887399e7e05fa95d3
Adam McFadden & Toby Alden <student@epicodus.com>
2015-06-16 15:11:36 -0700

    Added Promises to upcoming-bills.js

--- app/routes/upcoming-bills.js
+++ app/routes/upcoming-bills.js
@@ -2,13 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    var url = 'https://congress.api.sunlightfoundation.com/upcoming_bills?apikey=0613f3c5dde44d699a1a8c7adb2e6ed7&chamber=house&order=scheduled_at';
+
+    var url = 'http://congress.api.sunlightfoundation.com/upcoming_bills?&chamber=house&order=scheduled_at&apikey=0613f3c5dde44d699a1a8c7adb2e6ed7';
     return Ember.$.getJSON(url).then(function(responseJSON) {
-      var upcomingBills = [];
-      responseJSON.results.forEach(function(upcomingBill) {
-        upcomingBills.push(upcomingBill);
-      });
-      return upcomingBills;
+      return responseJSON.results;
+    }).then(function(bills) {
+      return Promise.all(bills.map(function(bill2) {
+        var url_bills = 'https://congress.api.sunlightfoundation.com/bills?apikey=0613f3c5dde44d699a1a8c7adb2e6ed7&bill_id=' + bill2.bill_id;
+        return Ember.$.getJSON(url_bills).then(function(responseJSON) {
+          return responseJSON.results[0];
+        });
+      }));
     });
+
   }
 });
